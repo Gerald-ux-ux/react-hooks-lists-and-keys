5d5afcf95db7f7b8169d934549fe1c9206d0b65b
gerald <you@example.com>
2022-10-07 09:04:10 +0300

    completed assignment

---
+++ src/components/Getcolor.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const GetColor = ({ colors }) => {
+  return colors.map((color) => {
+    return (
+      <li key={color} style={{ color: color }}>
+        {color}
+      </li>
+    );
+  });
+};
+
+export default GetColor;
